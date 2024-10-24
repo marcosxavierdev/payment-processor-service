d79fd93da8f2528a5ec787c3a691508061ed22a5
marcosxavierdev <sis.marcosxavier@gmail.com>
2024-10-24 12:22:20 -0300

    first commit

---
+++ src/test/java/com/consulting/payment_processor_service/PaymentProcessorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consulting.payment_processor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentProcessorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
